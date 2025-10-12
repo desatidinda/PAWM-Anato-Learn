0df722226411051470e36ae7e6de2995c1ecdf60
desatidinda <19623095@std.stei.itb.ac.id>
2025-10-12 20:02:35 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(cors());
 app.use(express.json());
 
