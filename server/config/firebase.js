23529e144e21d85e3b1906e0f55f7756016636f5
desatidinda <19623095@std.stei.itb.ac.id>
2025-11-01 00:03:40 +0700

    fix

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -7,11 +7,13 @@ try {
   if (process.env.FIREBASE_PRIVATE_KEY) {
     console.log('Using Firebase credentials from environment variables');
 
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+
     const serviceAccount = {
       type: "service_account",
       project_id: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      private_key: privateKey,
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -32,7 +34,7 @@ try {
   } 
 
   else {
-    console.log('🔧 Using Firebase credentials from serviceAccountKey.json');
+    console.log('Using Firebase credentials from serviceAccountKey.json');
     
     const serviceAccount = require('../serviceAccountKey.json');
     
